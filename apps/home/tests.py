3d4f80706743e45d0573ca8618dfd65c3b93391d
Serkan <serkanbulutgenc@gmail.com>
2025-02-20 08:37:39 +0000

    Added basic test for home app

--- apps/home/tests.py
+++ apps/home/tests.py
@@ -1,1 +1,17 @@
 # Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+
+class HomeAppTest(TestCase):
+    def setUp(self):
+        self.test_var = 'John'
+
+    def test_homeview_response(self):
+        res = self.client.get(reverse('home:index'))
+
+        self.assertEqual(res.status_code, 200)
+
+        self.assertContains(res, 'Home')
+
+        self.assertTemplateUsed(res, 'home/index.html')
